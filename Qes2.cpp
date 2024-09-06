a7f38161098f43dae04673db5b0a8e4fa5b939a3
Jyoti Sharma <officialjyotisharma2000@gmail.com>
2024-09-06 16:40:17 +0530

    first commit

---
+++ Qes2.cpp
@@ -0,0 +1,11 @@
+// Ques: Print 10 using 2 positive numbers less than 6  in C++ as output.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5, b = 5;
+    cout << a + b << endl;
+    return 0;
+}
---
+++ Qes5.cpp
@@ -0,0 +1,15 @@
+// Qes: WAP to find the circumference of a circle with radius 10 in C++.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int PI = 3.14;
+    int radius = 10;
+    int circumference = 2 * PI * radius;
+
+    cout << "Circumference is: " << circumference << endl;
+}
+
+
